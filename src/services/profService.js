732a30fd373cf9dd61970ac4e643537fe24d3bb9
Luam-Novais <luampabl014@gmail.com>
2025-09-16 22:00:50 -0300

    feat: implementar atualização de alunos e corrigir método de obtenção de todos os alunos

--- src/services/profService.js
+++ src/services/profService.js
@@ -1,39 +1,79 @@
 import prisma from '../config/prisma.js';
 import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken'
+import jwt from 'jsonwebtoken';
 export default class ProfService {
+
   static async profCreate({ name, username, password }) {
     const regexPassword = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{4,}$/;
     const verifyPassword = regexPassword.test(password);
     if (username < 3 || username > 20 || !verifyPassword) return { message: 'Formato dos dados enviados inválidos.' };
 
-    const profExisting = await prisma.professor.findUnique({where:{username:username}});
+    const profExisting = await prisma.professor.findUnique({ where: { username: username } });
     if (!profExisting) {
-
-      const prof = await prisma.professor.create({
-        data: {
-          name,
-          username,
-          password: await bcrypt.hash(password, 10),
-        },
-      });
-      const payload = {
-        name: prof.name
+      try {
+        const prof = await prisma.professor.create({
+          data: {
+            name,
+            username,
+            password: await bcrypt.hash(password, 10),
+          },
+        });
+        const payload = {
+          name: prof.name,
+        };
+        const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, { expiresIn: '1d' });
+        return { prof: prof.name, token };
+      } catch (error) {}
+    }
+  }
+  static async profLogin({ username, password }) {
+    console.log(username, password);
+    const profExisting = await prisma.professor.findUnique({ where: { username: username } });
+    try {
+      const comparedPass = await bcrypt.compare(password, profExisting.password);
+      if (profExisting && comparedPass) {
+        const payload = {
+          name: profExisting.name,
+        };
+        const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, { expiresIn: '1d' });
+        return { message: 'Login feito com sucesso', prof: profExisting.name, token };
       }
-      const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, {expiresIn: '1d'})
-      return {prof: prof.name, token};
+    } catch (error) {
+      return { message: 'Credenciais inválidas.' };
     }
   }
-  static async profLogin({username, password}){
-    console.log(username, password)
-    const profExisting = await prisma.professor.findUnique({where:{username:username}});
-    const comparedPass = await bcrypt.compare(password, profExisting.password)
-    if(profExisting && comparedPass){
-        console.log('fez login')        
+  static async createAluno({ name }) {
+    if (name) {
+      try {
+        const newAluno = await prisma.aluno.create({ data: { name: name } });
+        return newAluno
+      } catch (error) {
+        return { message: 'Ocorreu um erro ao criar o aluno. Tente novamente.' };
+      }
     }
   }
+  static async editAluno({id, name, status}){
+    const nameAluno = name
+    const statusAluno = status
+    const findAluno = await prisma.aluno.findUnique({where:{id:Number(id)}})
+    if(findAluno){
+      if(name || status){
+        const updatedAluno = await prisma.aluno.update({
+          where: {id: Number(id)},
+          data: {
+            name: nameAluno ? nameAluno : name,
+            status: statusAluno ? statusAluno : status
+          }
+        })
+        return {message: 'Aluno atualizado com sucesso.',updatedAluno}
+      }
+    }else{
+      return{message: 'Aluno não encontrado.'}
+    }
+  }
+
   static async getAllStudents() {
-    const allStudents = await prisma.professor.findMany();
+    const allStudents = await prisma.aluno.findMany();
     return allStudents;
   }
 }
