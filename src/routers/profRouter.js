7ddbaaaff53c43c7c9c7a714f85aa8d6483a859a
Luam-Novais <luampabl014@gmail.com>
2025-09-16 22:00:05 -0300

    feat: adicionar autenticação para rotas de criação e edição de alunos

--- src/routers/profRouter.js
+++ src/routers/profRouter.js
@@ -1,10 +1,13 @@
 import Router from 'express'
 import ProfController from '../controllers/profController.js'
+import authProf from '../middlewares/authMiddleware.js'
 
 const router = Router()
 
 router.post('/create-prof', ProfController.createProf)
 router.post('/login-prof', ProfController.loginProf)
-router.get('/get-all-students', ProfController.getAllStudents)
+router.post('/create-aluno', authProf, ProfController.createAluno)
+router.put('/edit-aluno/:id', authProf, ProfController.editAluno)
+router.get('/get-all-students', authProf, ProfController.getAllStudents)
 
 export default router
