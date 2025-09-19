24534af53d05f212729dcfe315022875cb7d705d
Luam-Novais <luampabl014@gmail.com>
2025-09-18 21:34:14 -0300

    feat: ajustar mensagens de resposta e formatar nome ao criar professor

--- src/controllers/profController.js
+++ src/controllers/profController.js
@@ -1,17 +1,16 @@
 import ProfService from "../services/profService.js"
-import AlunoService from "../services/alunoService.js"
 export default class ProfController{
 
     static async createProf(req, res){
         const {name, username, password} = req.body
         if(!name || !username || !password) return res.status(400).json({message: 'Verifique novamente os dados enviados.'})
         const prof = await ProfService.profCreate({name, username, password})
-        res.status(201).json({message: 'Professor criado com sucesso', prof: prof})
+        res.status(201).json(prof)
     }
     static async loginProf(req,res){
         const {username, password} = req.body
             if( !username || !password) return res.status(400).json({message: 'Verifique novamente os dados enviados.'})
             const prof = await ProfService.profLogin({username, password})
-            res.status(201).json({message: 'Login feito com sucesso', prof: prof})
+            res.status(201).json(prof)
     }
 }
--- src/services/profService.js
+++ src/services/profService.js
@@ -4,6 +4,7 @@ import jwt from 'jsonwebtoken';
 export default class ProfService {
 
   static async profCreate({ name, username, password }) {
+    const formatedName = name.toLowerCase().trim()
     const regexPassword = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{4,}$/;
     const verifyPassword = regexPassword.test(password);
     if (username < 3 || username > 20 || !verifyPassword) return { message: 'Formato dos dados enviados inválidos.' };
@@ -13,27 +14,28 @@ export default class ProfService {
       try {
         const prof = await prisma.professor.create({
           data: {
-            name,
+            name: formatedName,
             username,
             password: await bcrypt.hash(password, 10),
           },
         });
         const payload = {
           name: prof.name,
+          profId: prof.id
         };
         const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, { expiresIn: '1d' });
-        return { prof: prof.name, token };
+        return {message:'Professor criado com sucesso.', prof: prof.name, token };
       } catch (error) {}
     }
   }
   static async profLogin({ username, password }) {
-    console.log(username, password);
     const profExisting = await prisma.professor.findUnique({ where: { username: username } });
     try {
       const comparedPass = await bcrypt.compare(password, profExisting.password);
       if (profExisting && comparedPass) {
         const payload = {
           name: profExisting.name,
+          profId: profExisting.id
         };
         const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, { expiresIn: '1d' });
         return { message: 'Login feito com sucesso', prof: profExisting.name, token };
