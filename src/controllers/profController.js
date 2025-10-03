cd2483a6df41fe7d8a8c8f7e5c5ee54443203ac6
Luam-Novais <luampabl014@gmail.com>
2025-10-02 21:29:19 -0300

    fix: corrigir mensagens de erro e formatação no serviço de criação e login de professores

--- src/controllers/profController.js
+++ src/controllers/profController.js
@@ -6,6 +6,7 @@ export default class ProfController{
         const {name, username, password} = req.body
         if(!name || !username || !password) return res.status(400).json({message: 'Verifique novamente os dados enviados.'})
         const prof = await ProfService.profCreate({name, username, password})
+        if(prof.error) res.status(400).json(prof.message)
         res.status(201).json(prof)
     }
     static async loginProf(req,res){
--- src/services/profService.js
+++ src/services/profService.js
@@ -2,15 +2,14 @@ import prisma from '../config/prisma.js';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 export default class ProfService {
-
   static async profCreate({ name, username, password }) {
-    const formatedName = name.toLowerCase().trim()
+    const formatedName = name.toLowerCase().trim();
     const regexPassword = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{4,}$/;
     const verifyPassword = regexPassword.test(password);
-    if (username < 3 || username > 20 || !verifyPassword) return { message: 'Formato dos dados enviados inválidos.' };
+    if (username < 3 || username > 20 || !verifyPassword) return { error: true, message: 'Formato dos dados enviados inválidos.' };
 
     const profExisting = await prisma.professor.findUnique({ where: { username: username } });
-    if (!profExisting) {
+    if (profExisting) return {error: true, message: 'Nome de usuário ja em uso. Por favor crie outro!'}
       try {
         const prof = await prisma.professor.create({
           data: {
@@ -21,29 +20,29 @@ export default class ProfService {
         });
         const payload = {
           name: prof.name,
-          profId: prof.id
+          profId: prof.id,
         };
         const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, { expiresIn: '1d' });
-        return {message:'Professor criado com sucesso.', prof: prof.name, token };
+        return { message: 'Professor criado com sucesso.', prof: prof.name, token };
       } catch (error) {}
-    }
+  
   }
   static async profLogin({ username, password }) {
     const profExisting = await prisma.professor.findUnique({ where: { username: username } });
     try {
+      if (!profExisting) return { error: true, message: 'Credenciais inválidas.' };
+
       const comparedPass = await bcrypt.compare(password, profExisting.password);
 
-      if(!profExisting || !comparedPass) return {error: true, message: 'Credenciais inválidas.'}
-
-      if (profExisting && comparedPass) {
-        const payload = {
-          name: profExisting.name,
-          profId: profExisting.id
-        };
-        const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, { expiresIn: '1d' });
-        return { message: 'Login feito com sucesso', prof: profExisting.name, token };
-      }
+      if (!comparedPass) return { error: true, message: 'Credenciais inválidas.' };
+      const payload = {
+        name: profExisting.name,
+        profId: profExisting.id,
+      };
+      const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, { expiresIn: '1d' });
+      return { message: 'Login feito com sucesso', prof: profExisting.name, token };
     } catch (error) {
+      console.log(error);
       return { message: 'Credenciais inválidas.' };
     }
   }
